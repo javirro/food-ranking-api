bed625f1264cd10f2ab50db175d1ed20fd32a0ab
idcetim <javirofe96@gmail.com>
2023-06-17 08:24:15 +0200

    Add jsonWebAuthorization for deleteItem

--- delete-item/index.ts
+++ delete-item/index.ts
@@ -1,13 +1,29 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { DELETE_ITEM, UPDATE_AFTER_DELETE } from "../databaseHelpers/queries";
 const pg = require("pg");
+import manageAuthorization, {
+  ManageAuthorizationRes,
+} from "../auth/jsonWebToken";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
   const { table, id, position } = req.query;
+  const header = JSON.parse(req.headers["authorization"]);
+  const tokenSecret = req.headers["token"];
 
+  let authData: ManageAuthorizationRes;
+
+  try {
+    authData = manageAuthorization(tokenSecret, header);
+  } catch (err) {
+    console.log("AUTH ERROR.", err.message);
+    context.res = {
+      status: 403,
+      body: { error: err.message },
+    };
+  }
 
   const connectionError = (err) => {
     if (err) {
@@ -23,12 +39,12 @@ const httpTrigger: AzureFunction = async function (
   await client.connect(connectionError);
 
   try {
-    await client.query(DELETE_ITEM(table,id));
-    await client.query(UPDATE_AFTER_DELETE(table, position))
+    await client.query(DELETE_ITEM(table, id));
+    await client.query(UPDATE_AFTER_DELETE(table, position));
     client.end();
 
     context.res = {
-      body: JSON.stringify(`Deleted item with id: ${id}`),
+      body: JSON.stringify(authData.token),
     };
   } catch (error) {
     client.end();
