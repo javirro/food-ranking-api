4bed82a1bbba375ed8861edd5aecaf1514f63e2c
idcetim <javirofe96@gmail.com>
2023-06-17 08:27:04 +0200

    Add jsonWebToken management

---
+++ auth/jsonWebToken.ts
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+
+
+export interface ManageAuthorizationRes {
+  isAuthorized: boolean;
+  token: string;
+}
+
+const verifyCallBack = (error, authorizedData) => {
+  if (error) {
+    throw new Error("Incorrect token. Do not have access to this operation.");
+  } else {
+    return authorizedData;
+  }
+};
+const manageAuthorization = (
+  tokenSecret: string,
+  authHeader: { user: string; password: string }
+): ManageAuthorizationRes => {
+  if (tokenSecret) {
+    const data = jwt.verify(tokenSecret, process.env.SECRET, verifyCallBack);
+    return { token: tokenSecret, isAuthorized: true };
+  } else {
+    const { user, password } = authHeader;
+    const usersEnabled = process.env.users;
+
+    if (usersEnabled.includes(user) && password === process.env.password) {
+      const token = jwt.sign(user, process.env.SECRET, { expiresIn: "1h" });
+      return { token, isAuthorized: true };
+    } else {
+      throw new Error("Incorrect token. Do not have access to this operation.");
+    }
+  }
+};
+
+export default manageAuthorization;
